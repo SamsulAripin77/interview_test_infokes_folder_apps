4a495f097285fca7cf0e66141d19a0e58407ecac
SamsulAripin77 <arifins192@gmail.com>
2025-01-19 17:29:12 +0700

    add e2e test

--- FE/cypress/e2e/folderPanel.spec.js
+++ FE/cypress/e2e/folderPanel.spec.js
@@ -5,7 +5,7 @@ describe('Folder Panel', () => {
       cy.get('.panel-left').contains('Folder List'); // Pastikan ada judul folder list
       cy.get('.folder-header').should('have.length', 2); // Memastikan ada 2 folder di panel kiri
     });
-  
+
     it('should toggle subfolders when folder is clicked', () => {
       cy.visit('/');
       cy.get('.folder-header').first().click(); // Klik folder pertama untuk toggle
@@ -13,13 +13,19 @@ describe('Folder Panel', () => {
       cy.get('.folder-header').first().click(); // Klik lagi untuk menutup folder
       cy.get('.nested').should('not.be.visible'); // Pastikan subfolder hilang
     });
-  
+
     it('should display subfolders in the right panel when a folder is clicked', () => {
       cy.visit('/');
       cy.get('.folder-header').first().click(); // Klik folder pertama untuk membuka subfolder
-      cy.get('.panel-right').contains('Subfolders of folder A'); // Pastikan panel kanan menampilkan subfolder
+      cy.get('.panel-right').contains('Contents of Provinsi Jawa Barat'); // Pastikan panel kanan menampilkan subfolder
     });
-  
+
+    it('should display subfolders in the right panel when a folder is clicked', () => {
+      cy.visit('/');
+      cy.get('.folder-header').last().click(); // Klik folder pertama untuk membuka subfolder
+      cy.get('.panel-right').contains('Contents of Provinsi Jawa Tengah'); // Pastikan panel kanan menampilkan subfolder
+    });
+
     it('should toggle nested subfolders when a folder is clicked', () => {
       cy.visit('/');
       cy.get('.folder-header').first().click(); // Klik folder pertama untuk membuka subfolder
@@ -28,6 +34,5 @@ describe('Folder Panel', () => {
       cy.get('.nested .folder-header').first().click(); // Klik untuk menutup nested subfolder
       cy.get('.nested .nested').should('not.be.visible'); // Pastikan nested subfolder hilang
     });
-  
+
   });
-  
